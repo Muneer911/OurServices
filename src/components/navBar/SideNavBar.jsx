309fd514c879a43e1c2fcfe664c562b3d9a95fba
Muneer <ahmed.elsadig.ahmed@gmail.com>
2025-02-22 02:19:28 +0300

    Enhance NavBar and SideNaveBar with animation and styling

--- src/components/navBar/SideNavBar.jsx
+++ src/components/navBar/SideNavBar.jsx
@@ -1,44 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
 import assetts from "../../JsData/assetts";
 
 export default function Togglee() {
+  const controls = useAnimation();
   const [open, setOpen] = useState();
-
   const toggle = () => {
     setOpen(!open);
   };
 
-  return (
-    <div className=" flex justify-center ">
-      {open && (
-        <div className="bg-white shadow-black shadow-xl absolute left-0 top-0 w-[330px] h-screen flex flex-col gap-[70px] ">
-          <div className="mt-10 pl-5 w-[70px]">
-            <img src={assetts.ww} alt="" />
-          </div>
-          <div className="pl-5">
-            <ul className=" flex flex-col gap-7">
-              <li>
-                <a href="">Home</a>
-              </li>
-              <li>
-                <a href="">Bussiness Setup</a>
-              </li>
-              <li>
-                <a href="">Pricing</a>
-              </li>
-              <li>
-                <a href="">About Us</a>
-              </li>
-            </ul>
-          </div>
-          <div className="flex justify-center items-center">
-            <button className="contact-us ">Contact Us</button>
-          </div>
-        </div>
-      )}
+  useEffect(() => {
+    if (open) {
+      controls.start({ x: 0, transition: { type: "spring" } });
+    } else {
+      controls.start({ x: -350, transition: { type: "spring" } });
+    }
+  }, [toggle]);
 
-      <button onClick={toggle} className="">
-        <img src={open ? assetts.iconsClose : assetts.iconsmMenu} alt="" />
+  return (
+    <div className=" flex justify-center">
+      {/* {open && ( */}
+      <motion.div
+        animate={controls}
+        className="side bg-white shadow-black shadow-xl absolute left-[0]  top-0 w-[330px] h-screen flex flex-col gap-[70px] "
+      >
+        <div className="mt-10 pl-5 w-[70px]">
+          <img src={assetts.ww} alt="" />
+        </div>
+        <div className="pl-5">
+          <ul className=" flex flex-col gap-7">
+            <li className="loser">
+              <a href="">Home</a>
+            </li>
+            <li className="loser">
+              <a href="">Bussiness Setup</a>
+            </li>
+            <li className="loser">
+              <a href="">Pricing</a>
+            </li>
+            <li className="loser">
+              <a href="">About Us</a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex justify-center items-center">
+          <button className="contact-us ">Contact Us</button>
+        </div>
+      </motion.div>
+      {/* )} */}
+
+      <button onClick={toggle} className="flex justify-center items-center">
+        <motion.img
+          transition={{ type: "easeInOut" }}
+          animate={{ rotate: open ? 180 : 0 }}
+          src={open ? assetts.iconsClose : assetts.iconsmMenu}
+          alt=""
+        />
       </button>
     </div>
   );
