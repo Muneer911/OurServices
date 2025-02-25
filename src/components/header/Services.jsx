1b11adeaff0f4a804e1a385929c1952afdc8ccea
Muneer <ahmed.elsadig.ahmed@gmail.com>
2025-02-25 22:31:51 +0300

    Refactor Services component to remove unused props and clean up code; enhance styling for focused state

--- src/components/header/Services.jsx
+++ src/components/header/Services.jsx
@@ -2,10 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useInView, motion } from "framer-motion";
 import { useRef } from "react";
 
-export default function Services({ item, onFocus, desc }) {
+export default function Services({ item, desc }) {
   const [isFocused, setIsFocused] = useState(false);
-  const ref = useRef(null);
-  const isInView = useInView(ref);
 
   useEffect(() => {
     // Function to handle clicks outside the focused element
@@ -32,16 +30,6 @@ export default function Services({ item, onFocus, desc }) {
   const variants = {
     hover: { scale: 1.1 },
     tap: { scale: 1.2 },
-    focused: {
-      transition: { duration: 1, type: " spring " },
-      // zIndex: 100,
-      // background: "white",
-      // width: "400px",
-      // position: "fixed",
-      // top: "50%",
-      // left: "50%",
-      // transform: "translate(-50%, -50%)",
-    },
   };
 
   return (
@@ -50,7 +38,7 @@ export default function Services({ item, onFocus, desc }) {
       whileHover="hover"
       animate={isFocused ? "focused" : " "}
       onClick={handleFocus}
-      className={`secDownCard ${isFocused ? "flashing-text" : ""} `} // Ensure the grid-item class is applied
+      className={`secDownCard ${isFocused ? "flashing-text relative" : ""}`} // Ensure the grid-item class is applied
     >
       {isFocused ? (
         <motion.div
@@ -59,7 +47,6 @@ export default function Services({ item, onFocus, desc }) {
           transition={{ duration: 0.9 }}
           className="p-1 flex flex-col content-center center gap-2 relative"
         >
-          <h1 className="text-lg font-bold">{item}</h1>
           <p className="text-[13px] font-normal">{desc}</p>
         </motion.div>
       ) : (
