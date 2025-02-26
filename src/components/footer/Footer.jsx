3628ccc0a976a167da95e14885be6a5e7dfed663
Muneer <ahmed.elsadig.ahmed@gmail.com>
2025-02-26 10:50:07 +0300

    update Footer component for better styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import assetts from "../../JsData/assets";
 export default function Footer() {
   return (
-    <div className="footer px-8 flex flex-col lg:flex-row justify-between items-center h-[95px] border-t-[1px] border-gray-300 font-semibold ">
-      <div className="left flex gap-10">
-        <a className="nav-item" href="/">
+    <div className="footer px-8 flex flex-col lg:flex-row justify-between items-center h-[95px] border-t-[1px] border-gray-300">
+      <div className="left flex justify-start gap-10 w-[33%]">
+        <a className="nav-item font-normal" href="/">
           Careers
         </a>
-        <a className="nav-item" href="/">
+        <a className="nav-item font-normal" href="/">
           Contact
         </a>
       </div>
-      <div className="mid">
+      <div className="mid flex justify-center w-[33%]">
         <p>Copyright © 2025 Bloom Techno</p>
       </div>
-      <div className="right flex gap-5">
+      <div className="right flex justify-end gap-5 w-[33%]">
         <a href="/">
           {" "}
           <img src={assetts.facebook} alt="facebook" />
