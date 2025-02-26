c54fa3ad795d2c21d13230fa0335fbe92619ae84
Muneer <ahmed.elsadig.ahmed@gmail.com>
2025-02-26 10:58:18 +0300

    Refactor Footer component for responsive width adjustments

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import assetts from "../../JsData/assets";
 export default function Footer() {
   return (
     <div className="footer px-8 flex flex-col lg:flex-row justify-between items-center h-[95px] border-t-[1px] border-gray-300">
-      <div className="left flex justify-start gap-10 w-[33%]">
+      <div className="left flex justify-start gap-10 md:w-[33%]">
         <a className="nav-item font-normal" href="/">
           Careers
         </a>
@@ -11,10 +11,10 @@ export default function Footer() {
           Contact
         </a>
       </div>
-      <div className="mid flex justify-center w-[33%]">
+      <div className="mid flex justify-center md:w-[33%]">
         <p>Copyright © 2025 Bloom Techno</p>
       </div>
-      <div className="right flex justify-end gap-5 w-[33%]">
+      <div className="right flex justify-end gap-5 md:w-[33%]">
         <a href="/">
           {" "}
           <img src={assetts.facebook} alt="facebook" />
